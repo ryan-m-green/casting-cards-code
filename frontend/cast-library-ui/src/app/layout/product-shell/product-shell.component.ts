e93a4fcb1cf32fe74751a1b4cc9ecb05dab4159b
Ryan <savingthrowstudio.app@gmail.com>
2026-06-23 21:11:50 -0700

    Added missing links back to about page

--- frontend/cast-library-ui/src/app/layout/product-shell/product-shell.component.ts
+++ frontend/cast-library-ui/src/app/layout/product-shell/product-shell.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-product-shell',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './product-shell.component.html',
   styleUrl: './product-shell.component.scss'
 })
