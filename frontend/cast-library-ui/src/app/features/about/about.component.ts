bbb296e76347fd1cdd28dd0b62b3b472867a752f
Ryan <savingthrowstudio.app@gmail.com>
2026-06-23 21:38:07 -0700

    more video fixes

--- frontend/cast-library-ui/src/app/features/about/about.component.ts
+++ frontend/cast-library-ui/src/app/features/about/about.component.ts
@@ -1,8 +1,6 @@
-import { Component, ElementRef, ViewChild, AfterViewInit, OnDestroy } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import videojs from 'video.js';
-import 'video.js/dist/video-js.css';
 
 @Component({
   selector: 'app-about',
@@ -11,33 +9,12 @@ import 'video.js/dist/video-js.css';
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
-export class AboutComponent implements AfterViewInit, OnDestroy {
+export class AboutComponent {
   @ViewChild('demoVideo') videoRef!: ElementRef<HTMLVideoElement>;
-  player: any;
 
   constructor(private router: Router) {}
 
   navigateToJoin() {
     this.router.navigate(['/join']);
-  }
-
-  ngAfterViewInit() {
-    if (this.videoRef) {
-      this.player = videojs(this.videoRef.nativeElement, {
-        controls: true,
-        autoplay: true,
-        muted: true,
-        loop: true,
-        responsive: true,
-        preload: 'auto',
-        aspectRatio: '16:9'
-      });
-    }
-  }
-
-  ngOnDestroy() {
-    if (this.player) {
-      this.player.dispose();
-    }
   }
 }
