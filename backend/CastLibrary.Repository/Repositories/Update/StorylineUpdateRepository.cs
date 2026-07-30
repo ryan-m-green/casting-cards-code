d23867bdd2a92dc508a304dfb25a4ecb4ea9e456
Ryan <savingthrowstudio.app@gmail.com>
2026-07-30 15:46:04 -0700

    Fixing soundtrack deletion

--- backend/CastLibrary.Repository/Repositories/Update/StorylineUpdateRepository.cs
+++ backend/CastLibrary.Repository/Repositories/Update/StorylineUpdateRepository.cs
@@ -159,7 +159,7 @@ public class StorylineUpdateRepository(
                   WHERE elem::text != @SoundtrackId::text
               ),
               updated_at = @UpdatedAt
-              WHERE soundtrack_ids @> @SoundtrackId::jsonb";
+              WHERE soundtrack_ids @> @SoundtrackId::text::jsonb";
 
         logging.LogDbOperation(correlation.TraceId, spanId, "UPDATE", "campaign_storyline", @params);
 
